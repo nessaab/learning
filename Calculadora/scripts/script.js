877f9c2a30349cee0c69420a8e9214ce9380bc3f
Vanessa Barros <barros_nessa@hotmail.com>
2024-09-15 16:34:20 -0300

    Add files via upload

--- Calculadora/scripts/script.js
+++ Calculadora/scripts/script.js
@@ -6,26 +6,52 @@ function calcular(n1,n2){
 
 
     switch(selector){
-        case '+':
-            calculo = (n1 + n2)
-            document.getElementById("resultado").innerHTML = `O resultado da soma de ${n1} + ${n2} é = ${calculo}`
+        case '1':
+            calculo = (n1 - (n1 * 0.15)) 
+            if(Number.isNaN(n1) || n1<=0){
+                document.getElementById("resultado").innerHTML = `digite um valor acima de zero`
+                break
+            } else {
+                document.getElementById("resultado").innerHTML = `O total é ${calculo}`
+            break
+            }
     }
 
     switch(selector){
-        case '-':
-            calculo = (n1 - n2)
-            document.getElementById("resultado").innerHTML = `O resultado da subtraçao de ${n1} - ${n2} é = ${calculo}`
+        case '2':
+            calculo = (n1 - (n1 * 0.10))
+            if(Number.isNaN(n1) || n1<=0){
+                document.getElementById("resultado").innerHTML = `digite um valor acima de zero`
+                break
+            } else {
+                document.getElementById("resultado").innerHTML = `O total é ${calculo}`
+            break
+            }
     }
 
     switch(selector){
-        case '*':
-            calculo = (n1 * n2)
-            document.getElementById("resultado").innerHTML = `O resultado da multiplicaçao ${n1} * ${n2} é = ${calculo}`
+        case '3':
+            calculo = n1
+            parcela = Math.round (calculo / 2)
+            if(Number.isNaN(n1) || n1<=0){
+                document.getElementById("resultado").innerHTML = `digite um valor acima de zero`
+                break
+            } else {
+                document.getElementById("resultado").innerHTML = `O total é ${calculo} em duas parcelas de ${parcela}`
+            break
+            }
     }
 
     switch(selector){
-        case '/':
-            calculo = (n1 / n2)
-            document.getElementById("resultado").innerHTML = `O resultado da divisao de ${n1} / ${n2} é = ${calculo}`
+        case '4':
+            calculo = Math.round ((n1 + (n1 * 0.10)))
+            parcela = (calculo / n2)
+            if(Number.isNaN(n2) || n2<=0 || Number.isNaN(n1) || n1<=0 ){
+                document.getElementById("resultado").innerHTML = `digite um valor acima de zero`
+                break
+            } else {
+                document.getElementById("resultado").innerHTML = `O total é ${calculo} em ${n2} parcelas de ${parcela}` 
+                break    
+            }
     }
-}
+ }
